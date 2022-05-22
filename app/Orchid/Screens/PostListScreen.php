6db6d97f1832b326837cf8b30f05ffafd806ed19
mounkaila <mounkaila144@gmail.com>
2022-05-22 10:37:18 +0100

    init

---
+++ app/Orchid/Screens/PostListScreen.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Orchid\Screens;
+
+use App\Models\Post;
+use App\Orchid\Layouts\PostListLayout;
+use Orchid\Screen\Actions\Link;
+use Orchid\Screen\Screen;
+
+class PostListScreen extends Screen
+{
+    /**
+     * Query data.
+     *
+     * @return array
+     */
+    public function query(): iterable
+    {
+        return [
+            'posts' => Post::paginate()
+        ];
+    }
+
+    /**
+     * Display header name.
+     *
+     * @return string|null
+     */
+    public function name(): ?string
+    {
+        return 'PostListScreen';
+    }
+
+    /**
+     * Button commands.
+     *
+     * @return \Orchid\Screen\Action[]
+     */
+    public function commandBar(): iterable
+    {
+        return [
+            Link::make('Create new')
+                ->icon('pencil')
+                ->route('platform.post.edit')
+        ];
+    }
+
+    /**
+     * Views.
+     *
+     * @return \Orchid\Screen\Layout[]|string[]
+     */
+    public function layout(): iterable
+    {
+        return [
+            PostListLayout::class
+        ];
+    }
+
+}
